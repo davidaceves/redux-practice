fd1bef5610e032ef664c537abe421e1c172e2bd9
David <digital0david@gmail.com>
2020-06-23 22:59:46 -0400

    added thunk to store

--- friends/src/index.js
+++ friends/src/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
+import thunk from "redux-thunk";
 
 import reducer from "./reducers/reducer.js";
 
-const store = createStore(reducer);
+const store = createStore(reducer, applyMiddleware(thunk));
 const rootElement = document.getElementById("root");
 
 ReactDOM.render(
