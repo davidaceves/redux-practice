c1ffa875acd6b7b4a92efc8d4bbdeabeaf2b573d
David <digital0david@gmail.com>
2020-06-23 22:56:51 -0400

    setup basic reducer

--- friends/src/index.js
+++ friends/src/index.js
@@ -1,10 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+
+import reducer from "./reducers/reducer.js";
+
+const store = createStore(reducer);
+const rootElement = document.getElementById("root");
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </Provider>,
+  rootElement
 );
